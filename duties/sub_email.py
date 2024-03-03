e9f3c9728946bd31680543579824d58c1e226304
zhuyf <zhuyifei.phil@gmail.com>
2024-03-03 16:48:19 +0800

    refine duty crontab

--- duties/sub_email.py
+++ duties/sub_email.py
@@ -23,16 +23,21 @@ def get_next_groups(current_num):
 def get_group_emails(group):
     return [user.email for user in group.users.all() if user.email]
 
-def create_email_message(group):
+def create_email_message(group,subject):
     member_names = [f"{user.first_name} {user.last_name}" if user.first_name and user.last_name else user.username for user in group.users.all()]
     members_list = ", ".join(member_names)
-    message = f"Hi, you are on duty next week. Please be prepared. Your team members are: {members_list}."
+    message = f"Hello, it's your turn to be on duty next week.\nPlease communicate with your group members and complete the duty at {subject} by next Tuesday afternoon.\nYour team members are: {members_list}."
     return message
 
 from django.core.mail import send_mail
 
 def send_email_to_group(group, subject):
-    message = create_email_message(group)  # 创建包含成员信息的邮件正文
+    if subject == '507':
+        location = '1#507'
+    elif subject == 'lan':
+        location = "Lan\'s Office"
+    message = create_email_message(group, location)
+    subject = f'On duty notice - {location}'
     for user in group.users.all():
         send_mail(
             subject,
@@ -46,19 +51,19 @@ def send_reminder_emails(current_sequence):
     next_group, prev_group = get_next_groups(current_sequence)
 
     if next_group:
-        send_email_to_group(next_group, 'Next Week Duty Reminder - 507')
+        send_email_to_group(next_group, '507')
 
     if prev_group:
-        send_email_to_group(prev_group, 'Next Week Duty Reminder - Lan\'s Office')
+        send_email_to_group(prev_group, 'lan')
 
 from datetime import datetime, date
 
 def determine_sequence_logic():
-    year_start = date(datetime.now().year, 3, 1)
+    year_start = date(datetime.now().year, 3, 3)
     # 获取今天的日期
     today = date.today()
-    today = date(datetime.now().year, 3, 10)
-    # 计算今天是今年的第几周
+    #today = date(datetime.now().year, 3, 4)
+    # 计算今天是从year_start开始的第几周
     current_week = (today - year_start).days // 7 + 1
 
     # 获取组的总数
@@ -74,6 +79,9 @@ def determine_sequence_logic():
     return next_sequence
 
 def my_scheduled_job():
-    # 假设你有逻辑来确定current_sequence
     current_sequence = determine_sequence_logic()
     send_reminder_emails(current_sequence)
+
+
+
+
